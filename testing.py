c01a91ee6ef47757de5234d9d41774117a152a5f
Hasan <hasan@dbo.id>
2025-11-21 22:57:46 +0700

    fix

--- testing.py
+++ testing.py
@@ -19,8 +19,8 @@ filename = input("Enter filename: ")
 os.system(f"cat {filename}")
 
 # 3. Hardcoded Secrets
-api_key = os.getenv("AK")
-print("API KEY = ",api_key)
+ak = os.getenv("AK")
+print("AK = ",ak)
 #API_KEY = "TESTING"
 # 4. Weak Hash
 user_pass = input("Enter your password: ")
